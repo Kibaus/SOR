44883aad268213ad0a72d373d5a0c1d3149c5fd4
Kiyaan Merali <Kmerali2004@outlook.com>
2024-05-25 21:48:09 -0700

    Updating PlayerController Movement Script for isometric movement

---
+++ SOR_isometric/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    private Vector3 _input;
+
+    void Update()
+    {
+        GatherInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+    }
+
+    void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward);
+    }
+}
