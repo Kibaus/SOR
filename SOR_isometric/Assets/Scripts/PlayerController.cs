094308f1dd00bec096a905f9b48dcb32b2b4eac1
Kiyaan Merali <Kmerali2004@outlook.com>
2024-05-25 22:59:34 -0700

    Updated Movement per isometric camera | Project Saved

--- SOR_isometric/Assets/Scripts/PlayerController.cs
+++ SOR_isometric/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
+using VSCodeEditor;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
-    private Vector3 _input;
+    [SerializeField] private float _turnspeed = 360;
+    private Vector3 input01;
 
     void Update()
     {
         GatherInput();
+        Look();
     }
 
     void FixedUpdate()
@@ -20,11 +25,24 @@ public class PlayerController : MonoBehaviour
     }
     void GatherInput()
     {
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        input01 = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
     }
+
+    void Look()
+    {
+        if (input01 != Vector3.zero)
+        {
+            var relative = (transform.position + input01) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,rot, _turnspeed * Time.deltaTime);
+        }
+    }
+
+    
 
     void Move()
     {
-        _rb.MovePosition(transform.position + transform.forward);
+        _rb.MovePosition(transform.position + (transform.forward * input01.magnitude) * _speed * Time.deltaTime);
     }
 }
